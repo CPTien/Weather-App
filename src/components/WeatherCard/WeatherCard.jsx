99ae71595a613fb032ab569eda76760ed3f729a1
patricktien <patrick1996112@gmail.com>
2022-10-11 22:14:53 -0700

    update weather card css

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -5,23 +5,28 @@ import "./WeatherCard.css";
 
 
 function WeatherCard({ weather }) {
-// weather.map((w) => {return console.log(w)})
-console.log("this is from weather card", weather, typeof weather)
+
   return (
-    <div className="weather-card">
+    
+    <div className="container">
+      <div className="weather-card">
       {/* 1st method - to call the img from api */}
       <div className="weather-icon">{weather.weather? <img alt={weather.name} src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img> : null }</div>
 
       {/* 2nd method - to call the img from public local storage */}
       {/* <div>{weather.weather? <img src={`icons/${weather.weather[0].icon}.png`}></img> : null }</div> */}
-
-      <div className="Location-name">{weather.name}</div>
+      
+      <div className="location-name"> {weather.name}</div>
       {/* shift + option + 8 = ° */}
       {/* toFixed method rounds the number */}
       {/* cannot just to {weather.main.temp}, need to have the condition */}
-      <div className="temp">{weather.main ? <p>{weather.main.temp.toFixed()} °F</p> : null }</div>
-      <div>{weather.weather ? <p>{weather.weather[0].main}</p> : null }</div>
 
+      <div className="temp">{weather.main ? <p> {weather.main.temp.toFixed()} °F</p> : null }</div>
+
+      {/* <div>{weather.weather ? <p>{weather.weather[0].main}</p> : null }</div> */}
+      </div>
+
+      
       {/* if there is no search, do not show the detail button! */}
       {weather.name !== undefined &&      
       <div>
