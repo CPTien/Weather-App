316ee84aa6e5bcbe669c484146529c9da40c56e8
patricktien <patrick1996112@gmail.com>
2022-10-14 10:45:27 -0700

    update nav logout icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,15 @@ import {Link} from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 import "./NavBar.css";
 import MyImage from './Logo.png';
+// import { CiLogout } from "react-icons/ci";
+
+
+//new
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { FiLogOut } from 'react-icons/fi';
 
 export default function NavBar({user, setUser}) {
 
@@ -23,11 +32,34 @@ export default function NavBar({user, setUser}) {
       
       <Link className='nav nav-1 nav-no-icon nav-location-list' to="/locationList">Location List</Link>
       
-      <Link className='nav nav-2 nav-no-icon' to={""} onClick={handleLogOut}>Logout</Link>
+      <Link className='nav nav-2 logout-icon' to={""} onClick={handleLogOut}><FiLogOut /></Link>
+
 
       <span className='nav nav-2 welcome-message nav-no-icon' >Hi! {user.name}</span>
 
     </nav>
+    // <Navbar bg="light" expand="lg">
+    // <Container>
+    //     <Navbar.Brand href="/">mytodos</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //         <Nav className="me-auto">
+    //         <Nav.Link href="/todoPage">home</Nav.Link>
+    //         <Nav.Link href="/aboutPage">about</Nav.Link>
+    //         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+    //             <NavDropdown.Item href="/todoPage">todos</NavDropdown.Item>   
+    //             <NavDropdown.Item href="/notesPage">notes</NavDropdown.Item>
+    //             <NavDropdown.Item href="/notes/notesListPage/:id">notes list</NavDropdown.Item>
+    //             <NavDropdown.Divider />
+    //         </NavDropdown>
+    //         </Nav>
+    //     </Navbar.Collapse>
+
+    //     <span>Welcome, {user.name}</span>
+    //     &nbsp;&nbsp; <Link to={""} onClick={handleLogOut}>Logout</Link>
+    // </Container> 
+    // </Navbar>
+
 
   );
 }
