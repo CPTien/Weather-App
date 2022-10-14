1054364259a20b7aca7570fdc1222268e44f6ffb
patricktien <patrick1996112@gmail.com>
2022-10-14 11:25:24 -0700

    fix nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -38,25 +38,20 @@ export default function NavBar({user, setUser}) {
       <span className='nav nav-2 welcome-message nav-no-icon' >Hi! {user.name}</span>
 
     </nav>
-    // <Navbar bg="light" expand="lg">
-    // <Container>
-    //     <Navbar.Brand href="/">mytodos</Navbar.Brand>
+    
+    // <Navbar bsClass="aaa" bg="" expand="lg">
+    // <Container bsClass='bs-con'>
+    //     <Navbar.Brand href="/">Weather<img className="app-logo-nav" src={MyImage} alt=""/></Navbar.Brand>
     //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     //     <Navbar.Collapse id="basic-navbar-nav">
     //         <Nav className="me-auto">
-    //         <Nav.Link href="/todoPage">home</Nav.Link>
-    //         <Nav.Link href="/aboutPage">about</Nav.Link>
-    //         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-    //             <NavDropdown.Item href="/todoPage">todos</NavDropdown.Item>   
-    //             <NavDropdown.Item href="/notesPage">notes</NavDropdown.Item>
-    //             <NavDropdown.Item href="/notes/notesListPage/:id">notes list</NavDropdown.Item>
-    //             <NavDropdown.Divider />
-    //         </NavDropdown>
+    //         <Nav.Link href="/weathers">Weather</Nav.Link>
+    //         <Nav.Link href="/locationList">Search Location</Nav.Link>
     //         </Nav>
     //     </Navbar.Collapse>
 
-    //     <span>Welcome, {user.name}</span>
-    //     &nbsp;&nbsp; <Link to={""} onClick={handleLogOut}>Logout</Link>
+    //     <span>Hi, {user.name}</span>
+    //     &nbsp;&nbsp; <Link to={""} onClick={handleLogOut}><FiLogOut /></Link>
     // </Container> 
     // </Navbar>
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App/App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
